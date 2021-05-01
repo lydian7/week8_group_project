ee61ab46b0464410902aa9ec16b0569e2fd7a976
Martin Sutherland <martinsutherland1@gmail.com>
2021-05-01 11:46:56 +0100

    audio working

--- client/src/components/MusicGame.js
+++ client/src/components/MusicGame.js
@@ -3,13 +3,14 @@ import React from "react";
 const MusicGame = ({songList}) => {
 
 const handlePlay = (e) => {
+    console.log(e.target.value)
     const audio = new Audio(e.target.value)
     audio.play()
   
 }
 
     const songClips = songList.map((song, index) => {
-        return <button key={index} value={song.link[0].attributes.href} onClick={handlePlay}>Play</button>
+        return <button key={index} value={song.link[1].attributes.href} onClick={handlePlay}>Play</button>
 
         
       
