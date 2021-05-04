1ead64bbd9111bd32eefe698dd77509a3d027dc0
Martin Sutherland <martinsutherland1@gmail.com>
2021-05-04 16:08:43 +0100

    css updates

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,8 @@ function App() {
   return (
     <div className="App">
       
-      
+
+<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet"></link>
         <MusicContainer /> 
       
       
